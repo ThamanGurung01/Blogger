aff5ce749a5e34252fbaa99df2f873151d33213a
thamangurung01 <tgurung519@gmail.com>
2024-10-03 22:35:49 +0545

    login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import './styles/App.css'
 import Navbar from './components/Navbar'
-import Signup from './pages/Signup'
+// import Signup from './pages/Signup'
 // import Posts from './pages/Posts'
+import Login from "./pages/Login"
 function App() {
 
   return (
     <div className=''>
      <Navbar/>
      {/* <Posts/> */}
-     <Signup/>
+     {/* <Signup/> */}
+     <Login/>
     </div>
   )
 }
--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -21,7 +21,8 @@ const Form = ({formType}) => {
     const {register,handleSubmit,reset,formState:{errors,isSubmitting}}=useForm();
     const onSubmit=async(data)=>{
 try{
-    const profilePicture = document.getElementById("profile").files[0];
+    if(formType=="signup")
+    {const profilePicture = document.getElementById("profile").files[0];
     const formData=new FormData();
     formData.append("fullName",data.fullName);
     formData.append("email",data.email);
@@ -34,11 +35,12 @@ try{
         headers:{
             "Content-Type":"multipart/form-data",
         }
-     }).then((res)=>{
-        console.log("Successfully Inserted:",res);
+     }).then(()=>{
         reset();
         setImageUrl(fallBackImage);
-    });
+    });}else if(formType=="login"){
+        console.log("login");
+    }
 }catch(Err){
     console.log("error in signing up:",Err);
 }
@@ -46,9 +48,8 @@ try{
   return (
     <div>
         <form action="" onSubmit={handleSubmit(onSubmit)} >
-            <label htmlFor="profile">Profile Picture:</label><br /><input {...register("profilePicture")} type="file" id="profile" style={{display:"none"}} onChange={(e)=>handleFileChange(e)} />
-            <img src={imageUrl} width={"200px"} alt="profileView" onClick={handleFileClick}/>
-            {formType==="signup"?( <><input {...register("fullName",{
+            {formType==="signup"?( <><label htmlFor="profile">Profile Picture:</label><br /><input {...register("profilePicture")} type="file" id="profile" style={{display:"none"}} onChange={(e)=>handleFileChange(e)} />
+            <img src={imageUrl} width={"200px"} alt="profileView" onClick={handleFileClick}/><input {...register("fullName",{
                 required:"Name is required",
                 minLength:{
                     value:5,
@@ -77,11 +78,11 @@ try{
                 }
             })} type="password" placeholder="Password" /> <br />
             {errors.password&&<div>{errors.password.message}</div>}
-            <label>Gender:</label><input {...register("gender",{
+            {formType=="login"?"":(<><label>Gender:</label><input {...register("gender",{
                 required:"Choose your gender",
             })} id="male" type="radio" name="gender" value={"male"}/><label htmlFor="male">Male</label>
             <input {...register("gender")} id="female" type="radio" name="gender" value={"female"}/><label htmlFor="female">Female</label><br />
-            {errors.gender&&<div>{errors.gender.message}</div>}
+            {errors.gender&&<div>{errors.gender.message}</div>}</>)}
             <button disabled={isSubmitting} type="submit">{isSubmitting?"Submitting...":formType==="signup"?"Signup":"Login"}</button>
         </form>
     </div>
