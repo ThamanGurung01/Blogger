0501ab0cda432360461fab56e53c9fc088291495
thamangurung01 <tgurung519@gmail.com>
2024-10-03 15:58:27 +0545

    signup validation and form handling

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,16 +1,37 @@
 import {useForm} from "react-hook-form";
 import PropTypes from "prop-types";
+import { useState } from "react";
+
 const Form = ({formType}) => {
+    const [imageUrl,setImageUrl]=useState("http://localhost:8000/images/avatar.png");
+
+    const handleFileClick=()=>{
+        document.getElementById("profile").click();
+    }
+    const handleFileChange=(e)=>{
+        const profilePicture=e.target.files[0];
+        if (profilePicture) {
+            setImageUrl(URL.createObjectURL(profilePicture));
+        }
+    }
+
+//form
     const {register,handleSubmit,formState:{errors,isSubmitting}}=useForm();
     const onSubmit=async(data)=>{
+        const profilePicture = document.getElementById("profile").files[0];
+        if (profilePicture) {
+            data={...data,profilePicture:profilePicture};
+        }
         await new Promise((resolve)=>{
             setTimeout(resolve,1000)
-        })
+        });
         console.log(data);
     }
   return (
     <div>
-        <form action="" onSubmit={handleSubmit(onSubmit)}>
+        <form action="" onSubmit={handleSubmit(onSubmit)} >
+            <label htmlFor="profile">Profile Picture:</label><br /><input {...register("profilePicture")} type="file" id="profile" style={{display:"none"}} onChange={(e)=>handleFileChange(e)} />
+            <img src={imageUrl} alt="profileView" onClick={handleFileClick}/>
             {formType==="signup"?( <><input {...register("fullName",{
                 required:"Name is required",
                 minLength:{
