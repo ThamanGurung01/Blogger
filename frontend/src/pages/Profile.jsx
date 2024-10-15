fc022d5edfc2e0f3e95b0ae2aa0d4916300587af
thamangurung01 <tgurung519@gmail.com>
2024-10-15 14:22:01 +0545

    profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,8 +1,35 @@
-// import React from 'react'
+import { useEffect, useState } from "react";
+import { authCheck } from "../services/auth/authenticationCheck";
+
 
 const Profile = () => {
+  const backendUrl=import.meta.env.VITE_BackendUrl;
+  const [users,setUser]=useState(null);
+  const getUserData=async()=>{
+    const user=await authCheck();
+    if(user&&user.length!=0){
+      setUser(user.data);
+    }
+  }
+  useEffect(()=>{
+    if(!users){
+      getUserData();
+    }
+    console.log(users);
+  },[users])
   return (
-    <div>Profile</div>
+    <div>
+      {users&&
+      <div>
+<img src={backendUrl+users.profileImageURL} alt="profile"/>
+<span>Name: {users.fullName}</span> <br />
+<span>Email: {users.email}</span>
+      </div>}
+      <h1>Statistics</h1>
+      <div>
+        
+      </div>
+    </div>
   )
 }
 
